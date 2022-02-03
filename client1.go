c24e5c03ac1e4ea02ce57b888b83da5a906d5161
carlosam-resiliencesoft <carlosamoussou1@gmail.com>
2022-02-03 16:36:45 +0100

    updating

--- client1.go
+++ client1.go
@@ -30,6 +30,21 @@ func init() {
 	}
 }
 
+func cli(i int) {
+	client1 := http.Client{
+		Timeout: time.Duration(30) * time.Second,
+	}
+	resp1, err1 := client1.Get(*url)
+	fmt.Printf("connexion numero %T  reussie \n", i)
+	if err1 != nil {
+		fmt.Printf("Error:  %s", err1)
+		return
+	}
+	defer resp1.Body.Close()
+	//config1, err1 := ioutil.ReadAll(resp1.Body)
+
+}
+
 type Url struct {
 	Small_https_download_url string `json:"small_https_download_url"`
 	Large_https_download_url string `json:"large_https_download_url"`
@@ -39,6 +54,7 @@ type Config struct {
 	Version int `json:"version"`
 	Urls    Url `json:"urls"`
 }
+
 
 func main() {
 	flag.Parse()
@@ -50,9 +66,21 @@ func main() {
 		fmt.Printf("Error:  %s", err)
 		return
 	}
-	defer resp.Body.Close()
+	defer resp.Body.Close() 
 	config, err := ioutil.ReadAll(resp.Body)
 
+	for i := 0; i < 4; i++ {
+		go cli(i)
+	}
+	n :=0
+	ticker := time.NewTicker(1 *time.Second)
+	for t := range ticker.C {
+	 fmt.Println("Invoked at",t)
+	 n+=1
+	 if n==15 {
+	 	break
+	 	}
+	}
 	var conf Config
 	json.Unmarshal(config, &conf)
 
